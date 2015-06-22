1ee9a0d0f05e6e5bd57edce4dadbc4e64c89cd3d
Kevin Labtani <greenleaf1348@gmail.com>
2015-06-22 18:30:03 +0200

    user logout, cleanup main and migrate db

--- app/main/views.py
+++ app/main/views.py
@@ -1,32 +1,7 @@
-from datetime import datetime
-from flask import render_template, session, redirect, url_for, current_app
-from .. import db
-from ..models import User
-from ..email import send_email
+from flask import render_template
 from . import main
-from .forms import NameForm
 
 
-@main.route('/', methods=['GET', 'POST'])
+@main.route('/')
 def index():
-    form = NameForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.name.data).first()
-        if user is None:
-            user = User(username=form.name.data)
-            db.session.add(user)  # we autocommit!! see config
-            session['known'] = False
-            if current_app.config['FLASKY_ADMIN']:
-                send_email(current_app.config['FLASKY_ADMIN'], 'New User',
-                           'mail/new_user', user=user)
-        else:
-            session['known'] = True
-        session['name'] = form.name.data
-        return redirect(url_for('.index'))
-        # if you are in a view function of a blueprint or a rendered template and
-        # you want to link to another endpoint of the same blueprint, you can use
-        # relative redirects by prefixing the endpoint with a dot only
-    return render_template('index.html',
-                           form=form, name=session.get('name'),
-                           known=session.get('known', False),
-                           current_time=datetime.utcnow())
+    return render_template('index.html')
