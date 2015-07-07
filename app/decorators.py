8f8039d704582e2dc915587c266ff0f968de57b1
Kevin Labtani <greenleaf1348@gmail.com>
2015-07-07 15:53:58 +0200

    user roles

---
+++ app/decorators.py
@@ -0,0 +1,21 @@
+# For cases in which an entire view function needs to be made available
+# only to users with certain permissions
+from functools import wraps
+from flask import abort
+from flask.ext.login import current_user
+from .models import Permission
+
+
+def permission_required(permission):
+    def decorator(f):
+        @wraps(f)
+        def decorated_function(*args, **kwargs):
+            if not current_user.can(permission):
+                abort(403)
+            return f(*args, **kwargs)
+        return decorated_function
+    return decorator
+
+
+def admin_required(f):
+    return permission_required(Permission.ADMINISTER)(f)
--- app/main/__init__.py
+++ app/main/__init__.py
@@ -7,3 +7,13 @@ from . import views, errors
 # Like applications, blueprints can be defined all in a single file or can be created in a more
 # structured way with multiple modules inside a package. To allow for the greatest flexi‐
 # bility, a subpackage inside the application package will be created to host the blueprint.
+
+from ..models import Permission
+
+@main.app_context_processor
+def inject_permissions():
+    return dict(Permission=Permission)
+
+# Permissions may also need to be checked from templates, so the Permission class with
+# all the bit constants needs to be accessible to them. To avoid having to add a template
+# argument in every render_template() call, a context processor is used.
